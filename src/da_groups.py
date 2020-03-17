932ab71da076d037aa376d58e68074b7265293aa
Kathryn DiPippo <dipippo.k@gmail.com>
2020-03-16 23:45:10 -0400

    Work-in-progress adopting easier way to expand on da_groups.json

--- src/da_groups.py
+++ src/da_groups.py
@@ -11,19 +11,24 @@ class DeviantArtNAPI:
     """Class to handle making calls to the New DeviantArt API available for Eclipse."""
 
     base_uri = "https://www.deviantart.com/_napi/shared_api/deviation"
-    deviation_id = None
-    csrf_token = None
 
-    def __init__(self, deviation_url):
+    def __init__(self):
         """Initialze DeviantArtNAPI by fetching Chrome's DeviantArt-related cookies, extracting
         the csrf token, and storing the deviation_id to perform actions on.
 
         Arguments:
-            deviation_url {string} -- deviation URL, i.e. https://da.com/art/Artwork-Name-12345.
+            deviation_url {string} -- optional deviation URL, i.e. https://da.com/art/Art-12345.
         """
         self.cookies = browser_cookie3.chrome(domain_name='.deviantart.com')
-        self.set_deviation_id(deviation_url)
-        self.set_csrf(deviation_url)
+
+    def get_cookies(self):
+        """Return the ???
+
+        Returns:
+            [type] -- [description]
+        """
+        print(self.cookies)
+        return self.cookies
 
     def get_groups(self):
         """Prints information about a subset (~10) of groups the user is a member of."""
@@ -33,23 +38,29 @@ class DeviantArtNAPI:
         print(json.dumps(rjson, indent=2))
 
     def get_group_folders(self, group_id):
-        """Prints folder information for the provided group_id.
+        """Returns folder information for the provided group_id.
 
         Arguments:
             group_id {int} -- ID number for the group on DeviantArt.
+
+        Returns:
+            string -- Full text response from the API call.
         """
         group_folders_url = f"{self.base_uri}/group_folders?groupid={group_id}&type=gallery"
         response = requests.get(group_folders_url, cookies=self.cookies)
-        rjson = json.loads(response.text)
-        print(json.dumps(rjson, indent=2))
+        return response.text
 
-    def add_deviation_to_group(self, group_id, folder_id):
+    def add_deviation_to_group(self, group_id, folder_id, deviation_url):
         """Adds the provided deviation to the specified group's folder; prints status and text.
 
         Arguments:
             group_id {int} -- ID number for the group on DeviantArt.
             folder_id {int} -- ID number for the folder of the group on DeviantArt.
+            deviation_url {string} -- optional deviation URL, i.e. https://da.com/art/Art-12345.
         """
+        csrf_token = get_csrf(deviation_url, self.get_cookies())
+        deviation_id = get_deviation_id(deviation_url)
+
         group_add_url = f"{self.base_uri}/group_add"
         headers = {
             "accept": 'application/json, text/plain, */*',
@@ -59,43 +70,81 @@ class DeviantArtNAPI:
             "groupid": group_id,
             "type": "gallery",
             "folderid": folder_id,
-            "deviationid": self.deviation_id,
-            "csrf_token": self.csrf_token
+            "deviationid": deviation_id,
+            "csrf_token": csrf_token
         })
         response = requests.post(group_add_url, cookies=self.cookies, headers=headers, data=data)
         print(response.status_code)
         rjson = json.loads(response.text)
         print(json.dumps(rjson, indent=2))
 
-    def set_csrf(self, deviation_url):
-        """Parse the HTML of the deviation to get the csrf token in the page. It's stored as the
-        value of a hidden input with name 'validate_token'.
+def get_csrf(deviation_url, cookies):
+    """Parse the HTML of the deviation to get the csrf token in the page. It's stored as the
+    value of a hidden input with name 'validate_token'.
 
-        Arguments:
-            deviation_url {string} -- deviation URL, i.e. https://da.com/art/Artwork-Name-12345.
-        """
-        page = requests.get(deviation_url, cookies=self.cookies)
-        soup = BeautifulSoup(page.text, 'html.parser')
-        self.csrf_token = soup.find("input", {"name":"validate_token"})["value"]
+    Arguments:
+        deviation_url {string} -- deviation URL, i.e. https://da.com/art/Art-12345.
+    """
+    page = requests.get(deviation_url, cookies=cookies)
+    soup = BeautifulSoup(page.text, 'html.parser')
+    return soup.find("input", {"name":"validate_token"})["value"]
 
-    def set_deviation_id(self, deviation_url):
-        """Extract the deviation_id from the full deviantart image URL.
+def get_deviation_id(deviation_url):
+    """Extract the deviation_id from the full deviantart image URL.
 
-        Arguments:
-            deviation_url {string} -- deviation URL, i.e. https://da.com/art/Artwork-Name-12345.
-        """
-        url_parts = deviation_url.split("-")
-        self.deviation_id = url_parts[-1]
+    Arguments:
+        deviation_url {string} -- deviation URL, i.e. https://da.com/art/Art-12345.
+    """
+    url_parts = deviation_url.split("-")
+    return url_parts[-1]
+
+def getGroupRow(name, id):
+    row = f"'name': '{name}', 'id': {id}"
+    return row
+
+def getRow(name, id):
+    row = f"'name': '{name}', 'id': {id}, 'categories': []"
+    return "  { " + row + " },"
+
+def getGroupId(group_name):
+    group_url = f"https://www.deviantart.com/{group_name}"
+    page = requests.get(group_url)
+    soup = BeautifulSoup(page.text, 'html.parser')
+    return soup.find("div", {"name":"gmi-Gruser"})["gmi-id"]
 
 if __name__ == "__main__":
-    # working, only gives 10 - deviantart.get_groups()
-    # working - deviantart.get_group_folders(14718292)
-    # not working - deviantart.add_deviation_to_group(14718292, 63180668, 765976537)
-    DEVIATION_URL = "https://www.deviantart.com/pepper-wood/" + \
-        "art/Digital-Inktober-Test-2018-765976537"
-    CHECK = input("Press enter to confirm that the deviation in Eclipse is open in Chrome: ")
-    DEVIANTART = DeviantArtNAPI(DEVIATION_URL)
-    DEVIANTART.add_deviation_to_group(
-        40852213, # Candycorn-Kingdom
-        60854872  # Featured
-    )
+    # print("1) Provide a deviation URL and add to groups based on category")
+    # print("2) Provide a group name and fetch all the information needed to update da_groups.json")
+    # choice = input("Type your option: ")
+    choice = "2"
+    if choice == "1":
+        # example: https://www.deviantart.com/pepper-wood/art/Digital-Inktober-Test-2018-765976537
+        print("Please ensure that the deviation is open in Eclipse in Chrome before continuing.")
+        DEVIATION_URL = input("Paste deviation URL: ")
+        DEVIANTART = DeviantArtNAPI()
+        DEVIANTART.add_deviation_to_group(
+            40852213, # Candycorn-Kingdom
+            60854872, # Featured
+            "https://www.deviantart.com/pepper-wood/art/Digital-Inktober-Test-2018-765976537"
+        )
+    elif choice == "2":
+        group_name = input("Please provide the group name: ")
+        group_id = getGroupId(group_name)
+        print(getGroupRow(group_name, group_id))
+        DEVIANTART = DeviantArtNAPI()
+        # 4928626; 40852213
+        texty = DEVIANTART.get_group_folders(13075062)
+        daGroups = json.loads(texty)
+        # print(json.dumps(daGroups, indent=2))
+        print('"folders": [')
+        for folder in daGroups['results']:
+            print(getRow(folder['name'], folder['folderId']))
+        print(']')
+        # THIS IS NOW RETURNING BLANK RESULTS???
+        '''
+        {
+            "results": []
+        }
+        '''
+        # groups I want to fetch: pixelized--world, kawaii-explosion
+
