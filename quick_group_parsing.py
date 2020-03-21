5138c94bba080d3a1d007117ab198fd99ba3f5ae
Kathryn DiPippo <dipippo.k@gmail.com>
2020-03-20 22:26:53 -0400

    Removed FULL folders

--- quick_group_parsing.py
+++ quick_group_parsing.py
@@ -4,7 +4,7 @@ def addFolderCheck(folderName, groupName):
     lowerFolderName = folderName.lower()
     if lowerFolderName in ["journals", "journal"]:
         return False
-    if "journal" in lowerFolderName:
+    if "full" in lowerFolderName:
         result = input(f"{groupName} ::: '{folderName}'. Add? (y/n): ")
         if result == "y":
             return True
@@ -36,18 +36,30 @@ for group in eclipse_groups_listing["groups_information"]:
 print("Leave blank to delete folder")
 for group in eclipse_groups_listing["groups_information"]:
     newFolders = []
+    promptContinue = False
+    alreadyPrinted = False
+    print(f"\n⭐{group['group_name']}")
     for folder in group["folders"]:
-        if len(folder["categories"] == 0):
-            result = input(f"{group['group_name']} ::: '{folder['folder_name']}'. Category? : ")
+        if len(folder["categories"]) > 0:
+            newFolders.append(folder)
+        else:
+            if not alreadyPrinted:
+                for folderListing in group["folders"]:
+                    print(f"-- {folderListing['folder_name']}")
+                print("-------------------")
+                alreadyPrinted = True
+            promptContinue = True
+            result = input(f"'{folder['folder_name']}'. Category? : ")
             if result != "":
                 folder["categories"].append(result)
                 newFolders.append(folder)
     group["folders"] = newFolders
-    cont = input("Continue? 'EXIT to leave: ")
-    if cont == "EXIT":
-        break
+    if promptContinue:
+        cont = input("Continue? 'EXIT to leave: ")
+        if cont == "EXIT":
+            break
 
-json_file = open('eclipse_groups_listing2.json', "w")
+json_file = open('eclipse_groups_listing.json', "w")
 json_file.write(json.dumps(eclipse_groups_listing, indent=2))
 json_file.close()
 
