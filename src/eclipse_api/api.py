07f9781a42c01288f2f680e200945dc84aab458d
Kathryn DiPippo <kathryn.dipippo@acquia.com>
2021-07-26 16:25:39 -0400

    WIP: in progress flake8 fixes for api.py

--- src/eclipse_api/api.py
+++ src/eclipse_api/api.py
@@ -1,21 +1,20 @@
-"""Class to handle making calls to the New DeviantArt API available for Eclipse."""
+"""Class to handle making calls to the DeviantArt Eclipse API."""
 
 import json
-import browser_cookie3
-from bs4 import BeautifulSoup
-import requests
 
+import browser_cookie3
+import requests
+from bs4 import BeautifulSoup
 from models import EclipseFolder
 
+
 class DeviantArtEclipseAPI:
-    """Class to handle making calls to the New DeviantArt API available for Eclipse."""
+    """Class to handle making calls to the DeviantArt Eclipse API."""
 
     base_uri = "https://www.deviantart.com/_napi"
 
     def __init__(self):
-        """Initialze DeviantArt Eclipse API by fetching Chrome's DeviantArt-related cookies and
-        extracting the csrf token via webscraping.
-        """
+        """Initialize API by fetching Chrome's DeviantArt-related cookies."""
         self.cookies = browser_cookie3.chrome(domain_name='.deviantart.com')
 
     def get_cookies(self):
@@ -26,7 +25,7 @@ class DeviantArtEclipseAPI:
         """
         return self.cookies
 
-    def get_groups(self, username, offset, limit = 24):
+    def get_groups(self, username, offset, limit=24):
         """Return a subset of the user's joined DeviantArt groups.
 
         Args:
@@ -41,16 +40,19 @@ class DeviantArtEclipseAPI:
             ValueError: If `limit` is greater than 24.
         """
         if limit > 24:
-            raise ValueError("Limit must be equal to or below 24.")
+            raise ValueError('Limit must be equal to or below 24.')
 
         queries = {
-            "username": username,
-            "moduleid": "1761969747", # Static value that points to the "Get Members" block.
-            "offset": offset,
-            "limit": limit
+            'username': username,
+            'moduleid': '1761969747',  # "Get Members" block ID.
+            'offset': offset,
+            'limit': limit,
         }
-        groups_url = f"{self.base_uri}/da-user-profile/api/module/groups/members"
-        groups_url += get_query_string(queries)
+        groups_url = ''.join([
+            self.base_uri,
+            '/da-user-profile/api/module/groups/members',
+            query_string(queries),
+        ])
 
         response = requests.get(groups_url, cookies=self.cookies)
 
@@ -58,36 +60,43 @@ class DeviantArtEclipseAPI:
         return rjson
 
     def get_group_folders(self, group_id, deviation_url):
-        """Returns folder information for the provided group_id ONLY if the cookies stored are
-        of a user who is a member of the provided group_id.
+        """Returns folder information for the provided group_id ONLY, if
+         cookies belong to a member of the group.
 
         Args:
-            group_id (int): ID number for the group on DeviantArt.
+            group_id (int): Group ID.
 
         Returns:
-            EclipseFolder[]: A list of Eclipse Folder objects.
+            EclipseFolder[]: List of EclipseFolder objects.
 
         Raises:
             RuntimeError: If API call returns an error payload.
         """
-        group_folders_url = f"{self.base_uri}/shared_api/deviation/group_folders"
-        group_folders_url = get_query_string({"groupid": group_id, "type": "gallery"})
+        group_folders_url = ''.join([
+            self.base_uri,
+            '/shared_api/deviation/group_folders',
+            query_string({'groupid': group_id, 'type': 'gallery'}),
+        ])
         headers = {'referer': deviation_url}
-        response = requests.get(group_folders_url, cookies=self.cookies, headers=headers)
+        response = requests.get(
+            group_folders_url,
+            cookies=self.cookies,
+            headers=headers,
+        )
 
         folder_data = json.loads(response.text)
-        if "error" in folder_data:
-            raise RuntimeError(folder_data["errorDetails"])
+        if 'error' in folder_data:
+            raise RuntimeError(folder_data['errorDetails'])
         folders = [EclipseFolder(d) for d in folder_data['results']]
         return folders
 
     def add_deviation_to_group(self, group_id, folder_id, deviation_url):
-        """Adds the provided deviation to the specified group's folder; prints status and text.
+        """Submit deviation to the specified folder in group.
 
         Args:
-            group_id (int): ID number for the group on DeviantArt.
-            folder_id (int): ID number for the folder of the group on DeviantArt.
-            deviation_url (string): optional deviation URL, i.e. https://da.com/art/Art-12345.
+            group_id (int): Group ID.
+            folder_id (int): Folder ID.
+            deviation_url (string): Deviation URL.
 
         Returns:
             string: Success message of result.
@@ -98,43 +107,51 @@ class DeviantArtEclipseAPI:
         csrf_token = get_csrf(deviation_url, self.cookies)
         deviation_id = get_deviation_id(deviation_url)
 
-        group_add_url = f"{self.base_uri}/shared_api/deviation/group_add"
+        group_add_url = ''.join([
+            self.base_uri,
+            '/shared_api/deviation/group_add',
+        ])
         headers = {
-            "accept": 'application/json, text/plain, */*',
-            "content-type": 'application/json;charset=UTF-8'
+            'accept': 'application/json, text/plain, */*',
+            'content-type': 'application/json;charset=UTF-8'
         }
         data = json.dumps({
-            "groupid": group_id,
-            "type": "gallery",
-            "folderid": folder_id,
-            "deviationid": deviation_id,
-            "csrf_token": csrf_token
+            'groupid': group_id,
+            'type': 'gallery',
+            'folderid': folder_id,
+            'deviationid': deviation_id,
+            'csrf_token': csrf_token,
         })
 
-        response = requests.post(group_add_url, cookies=self.cookies, headers=headers, data=data)
+        response = requests.post(
+            group_add_url,
+            cookies=self.cookies,
+            headers=headers,
+            data=data,
+        )
 
         rjson = json.loads(response.text)
-        if "error" in rjson:
-            raise RuntimeError(rjson["errorDetails"])
-        if rjson["needsVote"]:
-            return "Deviation added to folder and automatically approved"
-        return "Deviation submitted to folder and pending moderator approval"
+        if 'error' in rjson:
+            raise RuntimeError(rjson['errorDetails'])
+        if rjson['needsVote']:
+            return 'Deviation added to folder and automatically approved'
+        return 'Deviation submitted to folder and pending moderator approval'
 
 def get_deviation_id(deviation_url):
     """Extract the deviation_id from the full deviantart image URL.
 
     Args:
-        deviation_url (string): deviation URL, i.e. https://da.com/art/Art-12345.
+        deviation_url (string): Deviation URL.
     """
-    url_parts = deviation_url.split("-")
+    url_parts = deviation_url.split('-')
     return url_parts[-1]
 
 def get_csrf(deviation_url, cookies):
-    """Parses the HTML of the deviation to get the csrf token in the page. It's stored as the
-    value of a hidden input with name 'validate_token'.
+    """Scrape deviation page to retrieve CSRF token stored as hidden input with
+    name 'validate_token'.
 
     Args:
-        deviation_url (string): deviation URL, i.e. https://da.com/art/Art-12345.
+        deviation_url (string): Deviation URL.
         cookie (http.cookiejar.CookieJar): .deviantart.com Cookie Jar.
 
     Returns:
@@ -142,16 +159,16 @@ def get_csrf(deviation_url, cookies):
     """
     page = requests.get(deviation_url, cookies=cookies)
     soup = BeautifulSoup(page.text, 'html.parser')
-    return soup.find("input", {"name":"validate_token"})["value"]
+    return soup.find('input', {'name':'validate_token'})['value']
 
-def get_query_string(query_dict):
-    """Convert a dictionary into a query string URI, i.e. ?query1=value&query2=value.
+def query_string(query_dict):
+    """Convert a dictionary into a query string URI.
 
     Args:
         query_dict (dict): Dictionary of query keys and values.
 
     Returns:
-        string: Query string.
+        string: Query string, i.e. ?query1=value&query2=value.
     """
     queries = [f"{key}={query_dict[key]}" for key in query_dict.keys()]
     queries_string = "&".join(queries)
--- test.py
+++
@@ -1,4 +0,0 @@
-def get_status_msg(group_name, folder_name, message):
-    return '{0} | {1} | {2}'.format(group_name, folder_name, message)
-
-print(get_status_msg("GROUP", '', "MESSAGE"))
